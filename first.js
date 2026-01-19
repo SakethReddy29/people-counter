6b50daa78b30e91a0e4a969c8f6e07e3bdfe831e
SakethReddy29 <168444222+SakethReddy29@users.noreply.github.com>
2026-01-19 19:00:04 +0000

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let saveEl= document.getElementById("save-el")
+console.log(countEl)
+let count = 0
+
+function increment() {
+    console.log("clicked")
+    count +=  1
+    countEl.textContent = count
+    console.log(count)
+}
+function save(){
+    let countStr = count +" - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    
+}
+ 
